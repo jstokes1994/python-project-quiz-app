ff88fb9258a07bfe14a42859a0b5172ca236cec8
Joe Stokes <joestokes1994@gmail.com>
2018-10-15 10:52:56 +0000

    added complete page when all riddles answered correctly

--- run.py
+++ run.py
@@ -29,20 +29,21 @@ def index():
         return redirect(request.form["username"])
     return render_template("index.html")
 
-@app.route('/<username>', methods=["GET", "POST"])
+@app.route('/<username>', methods=['GET', 'POST'])
 def start_game(username):
     question_number = 0
     if request.method == "POST":
         return redirect(url_for('ask_questions', username=username, question_number=question_number))
     return render_template("startgame.html", username=username)
     
-@app.route('/<username>/<question_number>', methods=["GET", "POST"])
+@app.route('/<username>/<question_number>', methods=['GET', 'POST'])
 def ask_questions(username, question_number):
     questions = get_all_questions()
     question = questions[int(question_number)][0]
     if request.method == "POST":
         user_answer = request.get_data()
-        answer = user_answer.split("=")[1]
+        convert_to_str = user_answer.decode() #Necessary with Python3
+        answer = convert_to_str.split("=")[1]
         if answer == questions[int(question_number)][1]:
             question_number = int(question_number) + 1
             incorrect_answers.clear()
@@ -53,19 +54,28 @@ def ask_questions(username, question_number):
             return redirect(url_for('failure_page', username=username, question_number=question_number, answer=answer))
     return render_template("riddle1.html", question=question, username=username, incorrect_answers=incorrect_answers)
     
-@app.route('/<username>/<question_number>/<answer>/correct', methods=["POST", "GET"])
+@app.route('/<username>/<question_number>/<answer>/correct', methods=['GET', 'POST'])
 def success_page(username, question_number, answer):
-    if request.method == "POST":
+    if request.method == "POST" and int(question_number) <= 7:
         return redirect(url_for('ask_questions', username=username, question_number=question_number))
+    elif int(question_number) > 7:
+        return redirect(url_for('quiz_complete', username=username))
     return render_template("success.html", username=username)
     
-@app.route('/<username>/<question_number>/<answer>/incorrect', methods=["POST", "GET"])    
+@app.route('/<username>/<question_number>/<answer>/incorrect', methods=['GET', 'POST'])    
 def failure_page(username, question_number, answer):
     if request.method == "POST":
         return redirect(url_for('ask_questions', username=username, question_number=question_number))
     return render_template("failure.html", username=username, incorrect_answer=answer)
+    
+@app.route('/<username>/complete', methods=['GET', 'POST'])
+def quiz_complete(username):
+    return render_template("completed.html")
 
-
+#Add a finishing page
+#Can we figure out the + thing?
+#Scoreboard
+#Style it
 
 #README - under impression we were not to use sessions
 # request.get_data() as this is putting in a multidict
